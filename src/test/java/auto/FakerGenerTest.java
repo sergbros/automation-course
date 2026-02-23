bca53b3da2ea0d5d5a9d16a48a423a383502adb5
sergbros <sergbros2@gmail.com>
2026-02-23 09:00:21 +0300

    Практическое задание 20: Применение современных подходов

---
+++ src/test/java/auto/FakerGenerTest.java
@@ -0,0 +1,37 @@
+package auto;
+
+import com.github.javafaker.Faker;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.Test;
+
+public class FakerGenerTest {
+    @Test
+    void mainTest() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+            //Ваш код
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+
+            // Генерация данных
+            //Ваш код
+            Faker faker = new Faker();
+            String fakeName = faker.name().fullName();
+
+            // Мокирование API
+            page.route("**/dynamic_content", route -> {
+                route.fulfill(new Route.FulfillOptions()
+                        //Ваш код
+                        .setStatus(200)
+                        .setBody(fakeName)
+                );
+            });
+
+            // Запуск теста
+            page.navigate("https://the-internet.herokuapp.com/dynamic_content");
+            //Ваш код
+            assert page.textContent("pre").equals(fakeName);
+        }
+    }
+}
